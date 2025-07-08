d0b20fc9937972b3ad6188fc08422875222393d6
Vicgital <vicgital@hotmail.com>
2025-07-07 22:03:10 -0600

    commit .net drive example

---
+++ DotNet/MongoDBDemos/MongoDB.Demos.Demo01/PersonModel.cs
@@ -0,0 +1,185 @@
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Demos.Demo01
+{
+    public class PersonModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("gender")]
+        public string Gender { get; set; }
+
+        [BsonElement("name")]
+        public Name Name { get; set; }
+
+        [BsonElement("location")]
+        public Location Location { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("login")]
+        public Login Login { get; set; }
+
+        [BsonElement("dob")]
+        public DateInfo Dob { get; set; }
+
+        [BsonElement("registered")]
+        public DateInfo Registered { get; set; }
+
+        [BsonElement("phone")]
+        public string Phone { get; set; }
+
+        [BsonElement("cell")]
+        public string Cell { get; set; }
+
+        [BsonElement("id")]
+        public IdInfo IdInfo { get; set; }
+
+        [BsonElement("picture")]
+        public Picture Picture { get; set; }
+
+        [BsonElement("nat")]
+        public string Nationality { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+
+    }
+
+    public class Name
+    {
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("first")]
+        public string First { get; set; }
+
+        [BsonElement("last")]
+        public string Last { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    
+    public class Location
+    {
+        [BsonElement("street")]
+        public string Street { get; set; }
+
+        [BsonElement("city")]
+        public string City { get; set; }
+
+        [BsonElement("state")]
+        public string State { get; set; }
+
+        [BsonElement("postcode")]
+        public int Postcode { get; set; }
+
+        [BsonElement("coordinates")]
+        public Coordinates Coordinates { get; set; }
+
+        [BsonElement("timezone")]
+        public Timezone Timezone { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class Coordinates
+    {
+        [BsonElement("latitude")]
+        public string Latitude { get; set; }
+
+        [BsonElement("longitude")]
+        public string Longitude { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class Timezone
+    {
+        [BsonElement("offset")]
+        public string? Offset { get; set; }
+
+        [BsonElement("description")]
+        public string? Description { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class Login
+    {
+        [BsonElement("uuid")]
+        public string? Uuid { get; set; }
+
+        [BsonElement("username")]
+        public string? Username { get; set; }
+
+        [BsonElement("password")]
+        public string? Password { get; set; }
+
+        [BsonElement("salt")]
+        public string? Salt { get; set; }
+
+        [BsonElement("md5")]
+        public string? Md5 { get; set; }
+
+        [BsonElement("sha1")]
+        public string? Sha1 { get; set; }
+
+        [BsonElement("sha256")]
+        public string? Sha256 { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class DateInfo
+    {
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class IdInfo
+    {
+        [BsonElement("name")]
+        public string? Name { get; set; }
+
+        [BsonElement("value")]
+        public string? Value { get; set; }  // can be null
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+    public class Picture
+    {
+        [BsonElement("large")]
+        public string? Large { get; set; }
+
+        [BsonElement("medium")]
+        public string? Medium { get; set; }
+
+        [BsonElement("thumbnail")]
+        public string? Thumbnail { get; set; }
+
+        public override string ToString() =>
+            System.Text.Json.JsonSerializer.Serialize(this);
+    }
+
+
+}
---
+++ DotNet/MongoDBDemos/MongoDB.Demos.Demo01/Program.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Demos.Demo01;
+
+
+var dbUserName = Environment.GetEnvironmentVariable("MONGODB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("MONGODB_PASSWORD");
+string username = Uri.EscapeDataString($"{dbUserName}");
+string password = Uri.EscapeDataString($"{dbPassword}");
+
+string connectionUri = $"mongodb+srv://{username}:{password}@vicgitalmongodbcluster.jsgjkh4.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB.Demos.Demo01";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+try
+{
+    var result = await client.ListDatabaseNamesAsync();
+    Console.WriteLine("List Databases:");
+    await result.ForEachAsync((database) => 
+    {
+        Console.WriteLine($"{database}");
+    });
+
+
+    var analyticsDatabase = client.GetDatabase("analytics");
+    Console.WriteLine("List Collections in analytics databse/...:");
+    var collctionNames = await analyticsDatabase.ListCollectionNamesAsync();
+    await collctionNames.ForEachAsync((collection) => 
+    {
+        Console.WriteLine($"{collection}");
+    });
+
+    Console.WriteLine("\n\n\n");
+
+    // Get a list of persons.
+    var filter = Builders<BsonDocument>.Filter.Eq("gender", "female");
+
+    var personsCollection = analyticsDatabase.GetCollection<BsonDocument>("persons");
+    
+    var personsCursor = await personsCollection.FindAsync<BsonDocument>(filter);
+    var person = personsCursor.FirstOrDefault();
+
+    Console.WriteLine(person.ToJson());
+
+
+
+
+
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
